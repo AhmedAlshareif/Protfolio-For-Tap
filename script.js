260307323a961e8104d6620b2f71f370a84260db
AhmedAlshareif <ahmedmoh.alshareif@gmail.com>
2023-10-02 17:21:02 +0300

    Enhancements of code structure and build the js file.

---
+++ script.js
@@ -0,0 +1,23 @@
+function twoSum(nums, target) {
+    const numMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+
+        numMap.set(nums[i], i);
+    }
+
+    return null;
+}
+//example
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+const result = twoSum(nums, target);
+console.log(result);
+
+// the Output is: [0, 1]
